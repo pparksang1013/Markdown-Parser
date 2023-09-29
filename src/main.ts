885168e97ecc3153d6896df6715288c2aa208e05
pparksang <pparksang1013@gmail.com>
2023-09-30 03:20:55 +0900

    🧩Refactoring Header tag
    반복패턴으로 변경

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import "./style.css";
 
-let markdown: string = `
-ABC가나다
+let markdown: string = `ABC가나다
+된건가?
 # h1
 ## h2
 ### h3
@@ -12,25 +12,17 @@ ABC가나다
 `;
 
 function parserMD(text: string): string {
-    // FIXME: 처음오는 개행문자만 p태그로 변환
-    // P tag
-    text = text.replace(/\n (.+)/, "<p>$1</p>");
-
     // FIXME: ^, + 기호가 오면 캡쳐링 그룹이 실행이 안된다.
     // Ul tag
     text = text.replace(/^\- (.+)|^\* (.+)|^\+ (.+)/gm, "<ul><li>$1</li></ul>");
 
     // Header tag
     text = text.replace(/^# (.+)/gm, "<h1>$1</h1>");
-    text = text.replace(/^## (.+)/gm, "<h2>$1</h2>");
-    text = text.replace(/^### (.+)/gm, "<h3>$1</h3>");
-    text = text.replace(/^#### (.+)/gm, "<h4>$1</h4>");
-    text = text.replace(/^##### (.+)/gm, "<h5>$1</h5>");
-    text = text.replace(/^###### (.+)/gm, "<h6>$1</h6>");
-
-    text = text.replace(/(.+)/gm, "<p>$1</p>");
-
-    return text;
+    text = text.replace(/^#{2} (.+)/gm, "<h2>$1</h2>");
+    text = text.replace(/^#{3} (.+)/gm, "<h3>$1</h3>");
+    text = text.replace(/^#{4} (.+)/gm, "<h4>$1</h4>");
+    text = text.replace(/^#{5} (.+)/gm, "<h5>$1</h5>");
+    text = text.replace(/^#{6} (.+)/gm, "<h6>$1</h6>");
 }
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
